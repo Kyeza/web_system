0b08dd9e08497f8ec38d52f27ef9a64b52ec500e
kyeza <kyezaarnold63@gmail.com>
2019-04-02 02:41:00 +0300

    - Templates for creating, editing, detail and list Payroll center Tax Brackets - List view for LST rates

--- support_data/urls.py
+++ support_data/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('organizations/', OrganizationListView.as_view(), name='organization-list'),
     path('organization/<int:pk>/', OrganizationDetailView.as_view(), name='organization-detail'),
     path('organization/<int:pk>/delete/', OrganizationDelete.as_view(), name='organization-delete'),
-    path('tax_bracket/add/', TaxBracketCreate.as_view(), name='tax-bracket-create'),
+    path('tax_brackets/add/', TaxBracketCreate.as_view(), name='tax-bracket-create'),
     path('tax_bracket<int:pk>/edit/', TaxBracketUpdate.as_view(), name='tax-bracket-update'),
-    path('tax_bracket/', TaxBracketListView.as_view(), name='tax-bracket-list'),
+    path('tax_brackets/', TaxBracketListView.as_view(), name='tax-bracket-list'),
     path('tax_bracket/<int:pk>/', TaxBracketDetailView.as_view(), name='tax-bracket-detail'),
 ]
